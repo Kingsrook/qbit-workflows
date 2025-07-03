c75f46117202fad18d9cb956013bee1257b6cb26
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2025-07-03 08:52:25 -0500

    Update version to 0.1.6, qqq to 0.26.0-integration-20250703-134330, and maven.compiler.release instead of .source

--- src/main/java/com/kingsrook/qbits/workflows/WorkflowsQBitProducer.java
+++ src/main/java/com/kingsrook/qbits/workflows/WorkflowsQBitProducer.java
@@ -72,7 +72,7 @@ public class WorkflowsQBitProducer implements QBitMetaDataProducer<WorkflowsQBit
       QBitMetaData qBitMetaData = new QBitMetaData()
          .withGroupId("com.kingsrook.qbits")
          .withArtifactId("workflows")
-         .withVersion("0.1.5")
+         .withVersion("0.1.6")
          .withNamespace(getNamespace())
          .withConfig(getQBitConfig());
 
